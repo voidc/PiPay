b83b691079dd0a424cf5b4ba446a5120bd1547f9
Dominik Stolz <dominik.stolz21@gmail.com>
2016-07-10 20:31:23 +0200

    Keystore entfernt

--- app/src/test/java/de/sjsolutions/pipay/ExampleUnitTest.java
+++ app/src/test/java/de/sjsolutions/pipay/ExampleUnitTest.java
@@ -2,11 +2,6 @@ package de.sjsolutions.pipay;
 
 import org.junit.Test;
 
-import java.math.BigInteger;
-import java.nio.charset.Charset;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -17,25 +12,4 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
-
-    @Test
-    public void testMd5() {
-        System.out.println(md5("moneyboy"));
-    }
-
-    public static String md5(String s) {
-        MessageDigest digest;
-        try {
-            digest = MessageDigest.getInstance("MD5");
-            digest.update(s.getBytes(Charset.forName("US-ASCII")), 0, s.length());
-            byte[] magnitude = digest.digest();
-            BigInteger bi = new BigInteger(1, magnitude);
-            String hash = String.format("%0" + (magnitude.length << 1) + "x", bi);
-            return hash;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
-
 }
